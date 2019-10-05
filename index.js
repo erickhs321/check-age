f6a83a3b4f0fc7af07efb5a1e59b88d0d3d8e8ae
Erick <erickhenrique321@gmail.com>
2019-10-04 22:26:32 -0300

    routes

--- index.js
+++ index.js
@@ -17,8 +17,24 @@ app.get('/', (req, res) => {
   return res.render('index')
 })
 
+app.get('/major', (req, res) => {
+  const { age } = req.query
+  return res.render('major', { age })
+})
+
+app.get('/minor', (req, res) => {
+  const { age } = req.query
+  return res.render('minor', { age })
+})
+
 app.post('/check', (req, res) => {
-  console.log(req.body)
+  const { age } = req.body
+
+  if (age >= 18) {
+    res.redirect(`/major?age=${age}`)
+  } else {
+    res.redirect(`/minor?age=${age}`)
+  }
 })
 
 app.listen(3000)
